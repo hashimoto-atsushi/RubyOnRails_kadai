3a8e62b98d65353b0e0c39d75c4629be89db4e27
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-09-22 17:46:50 +0900

    update index.html.erb, comments_controller.rb
    削除機能追加 destroyアクション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,4 +20,10 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to comments_path
+  end
+
 end
