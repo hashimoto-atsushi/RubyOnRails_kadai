ba89cffd0ca31b3dd541eae5126cbc359a34441f
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-09-22 16:32:32 +0900

    update comments_controller
    createメソッド追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,17 @@
 class CommentsController < ApplicationController
   def index
+
   end
 
   def new
     @comments = Comment.all
   end
+
+  def create
+   @comment = Comment.new(content: params[:comment][:content])
+   @comment.save
+   redirect_to comments_path
+ end
 
   def edit
   end
