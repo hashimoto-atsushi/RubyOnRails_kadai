cb800cc81e0bb059c954370729827e2218b9b631
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-09-22 18:16:29 +0900

    update edit.html.erb, comments_controller
    編集画面修正, アップデート機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,13 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.new(content: params[:comment][:content])
+    @comment.save
+    redirect_to comments_path
   end
 
   def show
